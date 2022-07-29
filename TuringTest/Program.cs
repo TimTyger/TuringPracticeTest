847bdd6fc5fcb50496f522408bf8ce082bd6b0a8
Adebo Timothy <adebortimothy@gmail.com>
2022-07-29 03:32:52 -0700

    Add project files.

---
+++ TuringTest/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TuringTest
+{
+   class Solution
+    {
+        public int CalPoints(string[] ops)
+        {
+            var newList = new List<int>(); int[] ints;
+            for (int i = 0; i < ops.Length-1; i++)
+            {
+                var count = newList.Count;
+                if (ops[i]=="C")
+                {
+                    newList.RemoveAt(count - 1);
+                }
+                else if (ops[i]=="D")
+                {
+                    newList.Add((newList[count-1])*2) ;
+                }
+                else if (ops[i]=="+")
+                {
+                    newList.Add(newList[count-1]+ newList[count - 2]) ;
+                }
+                else newList.Add(int.Parse(ops[i])) ;
+            }
+            ints = newList.ToArray();
+            return ints.Sum();
+        }
+
+        public bool IsValid(string s)
+        {
+            var array = s.ToCharArray();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if ((array[i] == ')' && array[i - 1] != '(')||(array[i] == '}' && array[i - 1] != '{')||(array[i] == ']' && array[i - 1] != '[')) return false;
+                //if (array[i] == '}' && array[i - 1] != '{') return false;
+               // if (array[i] == ']' && array[i - 1] != '[') return false;
+            }
+            return true;
+        }
+    }
+
+    class CalPoints
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            if (solution.IsValid(Console.ReadLine())) Console.WriteLine("valid");
+            else Console.WriteLine("invalid");
+            
+        }
+    }
+}
